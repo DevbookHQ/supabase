84dbc25b1e6dd68deb3f14b99e6af5b27b1ac80f
Vaclav Mlejnsky <vasek.mlejnsky@gmail.com>
2022-01-27 13:04:42 +0000

    Integrate Devbook [WIP]

--- web/src/components/DevbookEditor.js
+++ web/src/components/DevbookEditor.js
@@ -1,5 +1,6 @@
 import React, {
   useState,
+  useEffect,
 } from 'react'
 
 import {
@@ -20,9 +21,11 @@ export default function Editor({
   sql,
   filepath,
 }) {
-  const [code, setCode] = useState(initialCode)
-  const { runCmd, stdout, stderr } = useDevbook({ debug: true, env: Env.Supabase })
+  const [code, setCode] = useState('')
+  const { runCmd, stdout, stderr, fs, status } = useDevbook({ debug: true, env: Env.Supabase })
   
+  // fs?.get('/components/Auth.js').then(content => console.log({ content }))
+
   function run() {
     const cmd = `psql postgresql://postgres:572570920Vasa@db.axzsxyxaetidbdskvkov.supabase.co -c '${sql}'`
     console.log({ cmd })
@@ -31,23 +34,49 @@ export default function Editor({
 
   function updateCode(content) {
     setCode(content)
+    fs?.write(filepath, content)
   }
 
+  useEffect(function getFileContent() {
+    if (!fs) return
+    if (status !== DevbookStatus.Connected) return
+    if (!filepath) return
+
+    console.log('RETRIEVING FILE')
+    fs.get(filepath).then(content => { 
+      console.log('FILE CONTENT', { content })
+      setCode(content)
+    })
+  }, [status, fs, filepath])
+
   return (
-    <div className="dbk-editor-wrapper">
+    <div className="dbk-editor-wrapper">      
+      {!sql && !code && <span>Loading</span>}
       {sql && <button className="run-btn" onClick={run}>Run</button>}
-      <DevbookEditor
-        initialCode={initialCode || sql}
-        language={language}
-        onChange={updateCode}
-        filepath={filepath}
-      />
-      {sql && (stdout.length > 0 || stderr.length > 0) && (
-        <Output
-          stdout={stdout}
-          stderr={stderr}
-        />
+      {sql && (
+        <>
+          <DevbookEditor
+            initialCode={sql}
+            language={language}
+            onChange={updateCode}
+            filepath={filepath}
+          />
+          {(stdout.length > 0 || stderr.length > 0) && (
+            <Output
+              stdout={stdout}
+              stderr={stderr}
+            />
+          )}
+        </>
       )}
+      {code && 
+        <DevbookEditor
+          initialCode={code}
+          language={language}
+          onChange={updateCode}
+          filepath={filepath}
+        />
+      }
     </div>
   )
 }
